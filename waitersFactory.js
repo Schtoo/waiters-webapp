f322a72791d07072e7668628f974f186b108ec35
Schtoo <mgqobhoz@gmail.com>
2018-11-01 12:24:47 +0200

    added query for selecting waiter and it's working days

--- waitersFactory.js
+++ waitersFactory.js
@@ -16,19 +16,16 @@ module.exports = function (pool) {
         for (let i = 0; i < listOfDays.length; i++) {
             let day = listOfDays[i];
             let dayId = await pool.query('SELECT id FROM daysofweek WHERE day=$1', [day]);
-            console.log(dayId.rows[0].id);
+          // console.log(dayId.rows[0].id);
             await pool.query('INSERT INTO shifts (day_id, waiter_id) values ($1, $2)', [dayId.rows[0].id, waiter.id]);
         }
-        let shifts = await pool.query('select * from shifts');
-        return shifts.rows;
-    }
-    async function days() {
-        let weekdays = await pool.query('SELECT DISTINCT waiter FROM shifts JOIN waiters ON waiters.id = shifts.waiter_id');
+        let weekdays = await pool.query('SELECT DISTINCT waiters.waiter, daysofweek.day FROM waiters JOIN shifts ON waiters.id = shifts.waiter_id JOIN daysofweek ON shifts.day_id = daysofweek.id');
+        console.log(weekdays.rows);
         return weekdays.rows;
+
     }
     return {
         getWaiter,
-        assignShift,
-        days
+        assignShift
     }
 }
