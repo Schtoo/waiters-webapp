59e43deb87200d8fc1027a58f66a2d852dc3bb79
Schtoo <mgqobhoz@gmail.com>
2018-11-12 19:15:43 +0200

    added function to delete from shifts table

--- waitersFactory.js
+++ waitersFactory.js
@@ -6,27 +6,30 @@ module.exports = function (pool) {
             nameId = await pool.query('SELECT id FROM waiters WHERE waiter=$1', [names]);
         } else {
             nameId = await pool.query('SELECT id FROM waiters WHERE waiter=$1', [names]);
-        }        
+        }
         return nameId.rows[0];
     }
+
+    async function removingShifts (){
+        let shifts = await pool.query('DELETE FROM shifts');
+        return shifts.rows;
+    }
+
     //assigning shifts to waiters
     async function assignShift(waiterName, listOfDays) {
         // console.log(listOfDays)
         let waiter = await getWaiter(waiterName);
         // delete all the shifts for the current user
-        await pool.query('delete from shifts where waiter_id = $1',[waiter.id]);
+        await pool.query('delete from shifts where waiter_id = $1', [waiter.id]);
         //loop over the list of days 
         for (let i = 0; i < listOfDays.length; i++) {
             let day = listOfDays[i];
             let dayIdResults = await pool.query('SELECT id FROM daysofweek WHERE day=$1', [day]);
-            console.log('Nazi ii-days -------');
-            let dayId = dayIdResults.rows[0].id
-            console.log(dayIdResults.rows[0].id);
-            await pool.query('INSERT INTO shifts (day_id, waiter_id) values ($1, $2)', [dayId, waiter.id]);
+            await pool.query('INSERT INTO shifts (day_id, waiter_id) values ($1, $2)', [dayIdResults.rows[0].id, waiter.id]);
         }
     }
     //getting shifts for the waiter
-    async function getshifts(name){
+    async function getshifts(name) {
         let waiterDays = await pool.query(`SELECT DISTINCT waiters.waiter, daysofweek.day FROM waiters 
          JOIN shifts ON waiters.id = shifts.waiter_id
          JOIN daysofweek ON shifts.day_id = daysofweek.id WHERE waiter=$1`, [name]);
@@ -39,17 +42,17 @@ module.exports = function (pool) {
     }
 
     //reset everything in database
-    async function resetDb(){
+    async function resetDb() {
         let resetDatabase = await pool.query('DELETE * FROM shifts');
-        return resetDatabase.rows; 
+        return resetDatabase.rows;
     }
-    async function checkedDays(name){
+    async function checkedDays(name) {
         let weekdays = await getDays();
         let waiterShifts = await getshifts(name);
         for (let i = 0; i < weekdays.length; i++) {
             let dayElement = weekdays[i].day;
             for (let getshifts of waiterShifts) {
-                if(dayElement === getshifts.day){
+                if (dayElement === getshifts.day) {
                     weekdays[i].checked = "checked";
                 }
             }
@@ -61,6 +64,7 @@ module.exports = function (pool) {
         assignShift,
         getDays,
         checkedDays,
-        resetDb
+        resetDb,
+        removingShifts
     }
 }
